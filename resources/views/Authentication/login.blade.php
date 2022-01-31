e1136c5cce0835fed6939343be44252c6e01aff3
Ben sreyoun <sreyounben2019@gmail.com>
2022-01-31 14:06:40 +0700

    form login

--- resources/views/Authentication/login.blade.php
+++ resources/views/Authentication/login.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.blank')
+
 @section('content')
-    i am the contact page
-@stop
+    <div class="row text">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text-center">
+                        <h1 class="h4 text-gray-900 mb-4">EMP MANAGE</h1>
+                    </div>
+                    <form>
+                        <div class="mb-3">
+                            <label for="inputEmail3">Email</label>
+                            <input type="email" class="form-control" id="inputEmail3">
+                        </div>
+                        <div class="mb-3">
+                            <label for="inputPassword3">Password</label>
+                            <input type="password" class="form-control" id="inputPassword3">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-sm">login</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    @stop
