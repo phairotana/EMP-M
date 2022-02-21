06f8460e828c97f803c89b6f536a1ad744900f73
Ben sreyoun <sreyounben2019@gmail.com>
2022-02-21 13:02:39 +0700

    form detail

--- resources/views/Admin/Employees/show.blade.php
+++ resources/views/Admin/Employees/show.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.blank')
 @section('content')
+
+    <style>
+        .table-borderless td,
+        .table-borderless th{
+            border: none;
+        }
+    </style>
     <script href="https://cdnjs.com/libraries/ekko-lightbox"></script>
 
     <div class="col-md-12">
@@ -8,104 +15,92 @@
             <div class="card-header text-success font-weight-bold">
                 Detail data
             </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="container mt-2 mb-4 p-3 d-flex justify-content-center">
-                        <div class="card p-4">
-                            <div>
-                                <div class=" image d-flex flex-column justify-content-center align-items-center">
-                                    @if (empty($user->image))
-                                        <a href="{{ asset('uploads/files/default/default-image.png') }}"
-                                            data-lightbox="image-1"><img class="rounded-circle"
-                                                src="{{ asset('uploads/files/default/default-image.png') }}" height="100"
-                                                width="100" /></a>
-                                    @else
-                                        <a href="{{ asset($user->photo) }}" data-lightbox="image-1"><img
-                                                class="rounded-circle" src="{{ asset($user->photo) }}" height="100"
-                                                width="100" /></a>
-                                    @endif
-                                </div>
-                                {{-- <div class="d-flex flex-row justify-content-center align-items-center gap-2">
-                                    <span>{{$employee->name }}</span>
-                                </div class="d-flex flex-row justify-content-center align-items-center mt-3">
+            <div class="col-md-12 col-sm-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="container mt-2 mb-4 p-3 d-flex justify-content-center">
+                            <div class="card p-4">
                                 <div>
-                                    <span>{{$employee->email }}</span>
-                                </div> --}}
+                                    <div class=" image d-flex flex-column justify-content-center align-items-center">
+                                        @if (empty($employee->photo))
+                                            <a href="{{ asset('uploads/files/default/default-image.png') }}"
+                                                data-lightbox="image-1"><img class="rounded-circle"
+                                                    src="{{ asset('uploads/files/default/default-image.png') }}" height="100"
+                                                    width="100" /></a>
+                                        @else
+                                            <a href="{{ asset($employee->photo) }}" data-lightbox="image-1"><img
+                                                    class="rounded-circle" src="{{ asset($employee->photo) }}" height="100"
+                                                    width="100" /></a>
+                                        @endif
+                                    </div>
+                                    {{-- <div class="d-flex flex-row justify-content-center align-items-center gap-2">
+                                        <span>{{$employee->name }}</span>
+                                    </div class="d-flex flex-row justify-content-center align-items-center mt-3">
+                                    <div>
+                                        <span>{{$employee->email }}</span>
+                                    </div> --}}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="form-group p-4 mb-4">
-                    <div class="row">
-                        <div class="col">
-                            <strong>Name</strong>
-                            <table class="table table-striped">{{ $employee->name }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>Email</strong>
-                            <table class="table table-striped">{{ $employee->email }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>Address:</strong>
-                            <table class="table table-striped">{{ $employee->address }}</table>
-                        </div>
-                    </div>
-                    {{-- <div class="row">
-                        <div class="col">
-                            <strong>Department:</strong>
-                            <table class="table table-striped">{{$employee->department->title }}</table>
-                        </div>
-                    </div> --}}
-                    {{-- <div class="row">
-                        <div class="col">
-                            <strong>Salary Type:</strong>
-                            <table class="table table-striped">{{$employee->salary->salary }}</table>
-                        </div>
-                    </div> --}}
-
-                    <div class="row">
-                        <div class="col">
-                            <strong>Amount</strong>
-                            <table class="table table-striped">{{ $employee->amount }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>Join Date</strong>
-                            <table class="table table-striped">{{ $employee->join_date }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>End Date</strong>
-                            <table class="table table-striped">{{ $employee->end_date }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>Phone</strong>
-                            <table class="table table-striped">{{ $employee->phone }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>Detail</strong>
-                            <table class="table table-striped">{{ $employee->detail }}</table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <strong>Date of Birth</strong>
-                            <table class="table table-striped">{{ $employee->date_of_birth }}</table>
-                        </div>
+                    <div class="col-md-8">
+                        <table class="table table-borderless">
+                            <tr>
+                                <th style="width: 10%">Name</th>
+                                <td style="width: 2%">:</td>
+                                <td>{{ $employee->name }}</td>
+                            </tr>
+                            <tr>
+                                <th>Email</th>
+                                <td>:</td>
+                                <td>{{ $employee->email }}</td>
+                            </tr>
+                            <tr>
+                                <th>Address</th>
+                                <td>:</td>
+                                <td>{{ $employee->address }}</td>
+                            </tr>
+                            <tr>
+                                <th>Address</th>
+                                <td>:</td>
+                                <td>{{ $employee->address }}</td>
+                            </tr>
+                            <tr>
+                                <th>Department</th>
+                                <td>:</td>
+                                <td>{{ $employee->department->title }}</td>
+                            </tr>
+                            <tr>
+                                <th>Salary</th>
+                                <td>:</td>
+                                <td>{{ $employee->salary->salary }}</td>
+                            </tr>
+                            <tr>
+                                <th>Amount</th>
+                                <td>:</td>
+                                <td>{{ $employee->amount }}</td>
+                            </tr>
+                            <tr>
+                                <th>Join Date</th>
+                                <td>:</td>
+                                <td>{{ $employee->join_date }}</td>
+                            </tr>
+                            <tr>
+                                <th>End Date</th>
+                                <td>:</td>
+                                <td>{{ $employee->end_date }}</td>
+                            </tr>
+                            <tr>
+                                <th>Phone</th>
+                                <td>:</td>
+                                <td>{{ $employee->phone }}</td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 @endsection
+
