72542ff47025ccfa4f0917dd64e13cdf15f1ebcb
Ben sreyoun <sreyounben2019@gmail.com>
2022-03-22 14:29:47 +0700

    picture

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DepartmentController;
+use App\Http\Controllers\Api\EmployeeController;
 use App\Http\Controllers\Api\SalaryController;
-
+use App\Models\Employee;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,12 @@ Route::delete('/departments/{id}',[DepartmentController::class, 'destroy']);
 Route::get('salaries', [SalaryController::class, 'index']);
 Route::get('salary/{id}',[SalaryController::class, 'show']);
 Route::post('salaries', [SalaryController::class, 'store']);
-Route::get('salaries/{id}', [SalaryController::class, 'update']);
-Route::get('/salaries/{id}', [SalaryController::class, 'destroy']);
+Route::put('salaries/{id}', [SalaryController::class, 'update']);
+Route::delete('/salaries/{id}', [SalaryController::class, 'destroy']);
+
+Route::get('employee', [EmployeeController::class, 'index']);
+Route::post('employee', [EmployeeController::class, 'store']);
+Route::put('employee/{id}', [EmployeeController::class, 'update']);
+Route::delete('/employee/{id}', [EmployeeController::class, 'destroy']);
+Route::get('employee/{id}', [EmployeeController::class, 'show']);
 
